c57c3144d11962b9de0dc9fba72882c13c851db3
Shamble94 <fratus.nicholas@gmail.com>
2024-01-10 16:37:11 +0100

    Created AppHeader

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,21 @@
+<script>
+export default{
+    name: "AppHeader"
+}
+</script>
+<template lang="">
+  <div class="text-allineato">
+    <img src="/Yu-Gi-Oh!.svg" alt=""><h1> Yu-Gi-Oh API</h1>
+  </div>
+</template>
+<style lang ="scss">
+img{
+    width: 100px
+}
+.text-allineato{
+    display: flex;
+    align-items: center;
+    margin: 10px;
+    text-transform: uppercase;
+}
+</style>
